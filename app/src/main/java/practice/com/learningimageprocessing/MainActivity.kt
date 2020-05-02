8a6c0ddc8aba6b75daa8feb760933e08d245c0fb
tuanlq <tuan@funtap.vn>
2020-05-02 09:51:43 +0700

    add rotate image

--- app/src/main/java/practice/com/learningimageprocessing/MainActivity.kt
+++ app/src/main/java/practice/com/learningimageprocessing/MainActivity.kt
@@ -1,13 +1,14 @@
 package practice.com.learningimageprocessing
 
-import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import practice.com.learningimageprocessing.utils.ImageProcessingUtils
+import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +22,18 @@ class MainActivity : AppCompatActivity() {
         task.execute()
     }
 
-    class MyTask internal constructor(val context: Activity?, val drawable: Drawable?) :
+    class MyTask internal constructor(val context: MainActivity?, val drawable: Drawable?) :
         AsyncTask<Void, Void, Bitmap>() {
+        private val activityReference: WeakReference<MainActivity?> = WeakReference(context)
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            val activity = activityReference.get()
+            if (activity == null || activity.isFinishing) {
+                return
+            }
+            activity?.progressBar?.visibility = View.VISIBLE
+        }
 
         override fun doInBackground(vararg p0: Void?): Bitmap {
             val bitmap = ImageProcessingUtils.getBitmapFromDrawable(context, drawable)
@@ -32,17 +43,15 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: Bitmap?) {
             result?.let {
-                context?.let {
-                    context.runOnUiThread(object : Runnable {
-                        override fun run() {
-                            context.ivDest?.setImageBitmap(result)
-                        }
-
-                    })
+                val activity: MainActivity? = activityReference.get()
+                if (activity == null || activity.isFinishing) {
+                    return
                 }
-            }
+                activity.progressBar?.visibility = View.GONE
+                activity.ivDest?.setImageBitmap(result)
 
-            super.onPostExecute(result)
+                super.onPostExecute(result)
+            }
         }
 
     }
