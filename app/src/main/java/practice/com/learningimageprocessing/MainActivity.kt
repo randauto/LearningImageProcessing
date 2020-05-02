8fc70696f4d86ef4f6fdf67bfa7c1dc66aaeed69
tuanlq <tuan@funtap.vn>
2020-05-02 09:38:47 +0700

    add rotate image

--- app/src/main/java/practice/com/learningimageprocessing/MainActivity.kt
+++ app/src/main/java/practice/com/learningimageprocessing/MainActivity.kt
@@ -1,12 +1,49 @@
 package practice.com.learningimageprocessing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.AsyncTask
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import practice.com.learningimageprocessing.utils.ImageProcessingUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ivSource?.setImageDrawable(resources.getDrawable(R.drawable.dog));
+
+        val task = MyTask(this, resources.getDrawable(R.drawable.dog))
+        task.execute()
+    }
+
+    class MyTask internal constructor(val context: Activity?, val drawable: Drawable?) :
+        AsyncTask<Void, Void, Bitmap>() {
+
+        override fun doInBackground(vararg p0: Void?): Bitmap {
+            val bitmap = ImageProcessingUtils.getBitmapFromDrawable(context, drawable)
+
+            return ImageProcessingUtils.rotate(bitmap, 45.0f);
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            result?.let {
+                context?.let {
+                    context.runOnUiThread(object : Runnable {
+                        override fun run() {
+                            context.ivDest?.setImageBitmap(result)
+                        }
+
+                    })
+                }
+            }
+
+            super.onPostExecute(result)
+        }
+
     }
 }
---
+++ app/src/main/java/practice/com/learningimageprocessing/utils/ImageProcessingUtils.java
@@ -0,0 +1,77 @@
+package practice.com.learningimageprocessing.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by tuanlq on 5/2/20
+ */
+public class ImageProcessingUtils {
+    public static Bitmap getBitmapFromDrawable(Context context, Drawable drawable) {
+        Bitmap bitmap = null;
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    public static Bitmap rotate(Bitmap src, float degree) {
+        // create new matrix
+        Matrix matrix = new Matrix();
+        // setup rotation degree
+        matrix.postRotate(degree);
+
+        // return new bitmap rotated using matrix
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
+    }
+
+    public static Bitmap doHighlightImage(Bitmap src) {
+        // create new bitmap, which will be painted and becomes result image
+        Bitmap bmOut = Bitmap.createBitmap(src.getWidth() + 96, src.getHeight() + 96, Bitmap.Config.ARGB_8888);
+        // setup canvas for painting
+        Canvas canvas = new Canvas(bmOut);
+        // setup default color
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
+
+        // create a blur paint for capturing alpha
+        Paint ptBlur = new Paint();
+        ptBlur.setMaskFilter(new BlurMaskFilter(15, BlurMaskFilter.Blur.NORMAL));
+        int[] offsetXY = new int[2];
+        // capture alpha into a bitmap
+        Bitmap bmAlpha = src.extractAlpha(ptBlur, offsetXY);
+        // create a color paint
+        Paint ptAlphaColor = new Paint();
+        ptAlphaColor.setColor(0xFFFFFFFF);
+        // paint color for captured alpha region (bitmap)
+        canvas.drawBitmap(bmAlpha, offsetXY[0], offsetXY[1], ptAlphaColor);
+        // free memory
+        bmAlpha.recycle();
+
+        // paint the image source
+        canvas.drawBitmap(src, 0, 0, null);
+
+        // return out final image
+        return bmOut;
+    }
+
+}
